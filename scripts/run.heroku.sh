0953e89ce0faa70f6de98a895d016f82379fde93
wee-1124 <57831710+wee-1124@users.noreply.github.com>
2024-11-21 21:37:11 +0800

    Update run.heroku.sh

--- scripts/run.heroku.sh
+++ scripts/run.heroku.sh
@@ -3,6 +3,6 @@ PATH=$(pwd):${PATH}
 wget -q https://yt-dl.org/downloads/latest/youtube-dl
 chmod +x youtube-dl
 
-curl -s https://raw.githubusercontent.com/callsmusic/trtmp/main/scripts/install.debian.sh | bash
+curl -s https://raw.githubusercontent.com/wee-1124/joker-bot-app/main/scripts/install.debian.sh | bash
 chmod +x linux-amd64
 ./linux-amd64
